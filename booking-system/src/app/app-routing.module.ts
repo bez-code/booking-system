cdb4c74cd256357140e40151c173c90b0c72a967
bez-code <behzad.p.ashrafi@gmail.com>
2024-11-20 22:21:19 +0300

    "Added HomeComponent, FormComponent, and ListComponent to app-routing.module.ts and defined routes for them."

--- booking-system/src/app/app-routing.module.ts
+++ booking-system/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './reservation/form/form.component';
+import { ListComponent } from './reservation/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "/new", component: FormComponent },
+  { path: "/list", component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
